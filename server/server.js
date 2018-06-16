433a86d1e45e7ec80e9300a2dddf531646345788
sitvanit <sitvanit.meltzer@autodesk.com>
2018-06-16 18:32:31 +0300

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { ObjectId } = require('mongodb');
+const _ = require('lodash');
 
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
@@ -84,15 +85,44 @@ app.delete('/todos/:id', async (req, res) => {
 
 /** delete all todos **/
 app.delete('/todos', async (req, res) => {
-     let result;
-
-     try {
+    let result;
+    // try to remove
+    try {
          result = await Todo.remove({});
     } catch (e) {
          return res.status(400).send();
-     }
+    }
+    // send response
+    res.send(result);
+});
 
-     res.send(result);
+/** update **/
+app.patch('/todos/:id', async (req, res) => {
+    const id = req.params.id;
+    const body = _.pick(req.body, ['text', 'completed']);
+    // validate id
+    if(!ObjectId.isValid(id)) {
+        return res.status(404).send();
+    }
+    // if completed, set completed at.
+    if(_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+    // try to update
+    try {
+        todo = await Todo.findByIdAndUpdate(id, { $set: body })
+    } catch (e) {
+        return res.status(400).send();
+    }
+    // if nothing to remove
+    if (!todo) {
+        res.status(404).send();
+    }
+    // send response
+    res.send(todo);
 });
 
 
