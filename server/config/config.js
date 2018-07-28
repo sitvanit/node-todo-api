8f5be1156f436b79d5a5db0eb3c291cc07c7ce5b
sitvanit <sitvanit.meltzer@autodesk.com>
2018-07-28 20:48:43 +0300

    Add jwt to package.json

--- server/config/config.js
+++ server/config/config.js
@@ -15,3 +15,18 @@ if (env === 'development' || env === 'test') {
 // heroku config:set JWT_SECRET=gfdsgfdsgnk5jtlinoprjmroenglkfdnglfdkngor21 -> set env var
 // heroku config:unset JWT_SECRET -> delete env var
 // heroku config:get JWT_SECRET -> get env var
+
+// heroku config:get MONGODB_URI
+// mongodb://heroku_pw2k8zxz:vp68nf8cd9dukms9nsv7pa08r8@ds263520.mlab.com:63520/heroku_pw2k8zxz
+
+// mongodb:// -> a protocol - a mongodb connection request
+// heroku_pw2k8zxz -> user name
+// :
+// vp68nf8cd9dukms9nsv7pa08r8 -> password
+// @
+// ds263520.mlab.com -> the address itself
+// :
+// 63520 -> port
+// /
+// heroku_pw2k8zxz -> the DB
+
